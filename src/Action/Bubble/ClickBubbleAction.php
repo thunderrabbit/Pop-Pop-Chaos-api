630f41cdd176ffc5c54998f344d13f0f0e90869d
Rob Nugen <rob@robnugen.com>
2021-11-19 16:00:37 +0900

    Rename variable
    hopefully accurately this time.

--- src/Action/Bubble/ClickBubbleAction.php
+++ src/Action/Bubble/ClickBubbleAction.php
@@ -33,13 +33,13 @@ final class ClickBubbleAction
       $version = (int)$args['version'];
 
       $bubble_data = (array)$request->getParsedBody();
-      $bubble_array = json_decode($bubble_data["entry"]);
-      $bubble_array->radius = $bubble_array->radius + 1;   // MUST check DB otherwise can be hacked from frontend
+      $bubble_object = json_decode($bubble_data["entry"]);
+      $bubble_object->radius = $bubble_object->radius + 1;   // MUST check DB otherwise can be hacked from frontend
 
       ////   FOR NOW JUST TESTING IF THIS SAVES, bUT WE STILL MUST READ FROM DB TO SEEW AHT SIZE TO MAKE RADIUS
-      $this->bubbleClicker->clickBubble($bubble_array->bubble_id, (array)$bubble_array);
+      $this->bubbleClicker->clickBubble($bubble_object->bubble_id, (array)$bubble_object);
       // We must json encode the return_data https://www.slimframework.com/docs/v4/objects/response.html#returning-json
-      $payload = json_encode($bubble_array);
+      $payload = json_encode($bubble_object);
       $response->getBody()->write($payload);
       return $response
                 ->withHeader('Access-Control-Allow-Origin', 'https://bcn.robnugen.com')
