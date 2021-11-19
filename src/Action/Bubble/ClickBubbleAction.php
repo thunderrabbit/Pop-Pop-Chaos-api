c283433cdc97d31c3c7b4a8ab22224c5a2494403
Rob Nugen <rob@robnugen.com>
2021-11-19 16:17:29 +0900

    Bubble Click increase radius based on DB wooooohoooo

--- src/Action/Bubble/ClickBubbleAction.php
+++ src/Action/Bubble/ClickBubbleAction.php
@@ -3,6 +3,7 @@
 namespace App\Action\Bubble;
 
 use App\Domain\Bubble\Service\BubbleClicker;
+use App\Domain\Bubble\Service\BubbleReader;
 use App\Responder\Responder;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -19,10 +20,11 @@ final class ClickBubbleAction
      * @param Responder $responder The responder
      * @param BubbleClicker $bubbleClicker The service
      */
-    public function __construct(Responder $responder, BubbleClicker $bubbleClicker)
+    public function __construct(Responder $responder, BubbleClicker $bubbleClicker, BubbleReader $bubbleReader)
     {
         $this->responder = $responder;
         $this->bubbleClicker = $bubbleClicker;
+        $this->bubbleReader = $bubbleReader;
     }
 
     public function __invoke(
@@ -34,12 +36,13 @@ final class ClickBubbleAction
 
       $bubble_data = (array)$request->getParsedBody();
       $bubble_object = json_decode($bubble_data["entry"]);
-      $bubble_object->radius = $bubble_object->radius + 1;   // MUST check DB otherwise can be hacked from frontend
 
-      ////   FOR NOW JUST TESTING IF THIS SAVES, bUT WE STILL MUST READ FROM DB TO SEEW AHT SIZE TO MAKE RADIUS
-      $this->bubbleClicker->clickBubble($bubble_object->bubble_id, (array)$bubble_object);
+      $bubble_from_db = $this->bubbleReader->getBubbleData($bubble_object->bubble_id);
+      $bubble_from_db->radius += 1;       // TODO make this increase based on bubble and click details
+      ////  YAY THIS SAVES, TODO cleanup    Vars named "clicker" below are actually just updaters
+      $this->bubbleClicker->clickBubble($bubble_object->bubble_id, (array)$bubble_from_db);
       // We must json encode the return_data https://www.slimframework.com/docs/v4/objects/response.html#returning-json
-      $payload = json_encode($bubble_object);
+      $payload = json_encode($bubble_from_db);
       $response->getBody()->write($payload);
       return $response
                 ->withHeader('Access-Control-Allow-Origin', 'https://bcn.robnugen.com')
