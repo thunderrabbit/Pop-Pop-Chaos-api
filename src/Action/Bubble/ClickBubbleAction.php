5bc8c8bcccb7340df24a7a8b95919fc95a7c1c75
Rob Nugen <rob@robnugen.com>
2021-11-19 16:00:01 +0900

    Try using BubbleClicker service

--- src/Action/Bubble/ClickBubbleAction.php
+++ src/Action/Bubble/ClickBubbleAction.php
@@ -31,11 +31,14 @@ final class ClickBubbleAction
         array $args
     ): ResponseInterface {
       $version = (int)$args['version'];
-      // We must json encode the return_data https://www.slimframework.com/docs/v4/objects/response.html#returning-json
-      $return_data = array();
+
       $bubble_data = (array)$request->getParsedBody();
       $bubble_array = json_decode($bubble_data["entry"]);
       $bubble_array->radius = $bubble_array->radius + 1;   // MUST check DB otherwise can be hacked from frontend
+
+      ////   FOR NOW JUST TESTING IF THIS SAVES, bUT WE STILL MUST READ FROM DB TO SEEW AHT SIZE TO MAKE RADIUS
+      $this->bubbleClicker->clickBubble($bubble_array->bubble_id, (array)$bubble_array);
+      // We must json encode the return_data https://www.slimframework.com/docs/v4/objects/response.html#returning-json
       $payload = json_encode($bubble_array);
       $response->getBody()->write($payload);
       return $response
