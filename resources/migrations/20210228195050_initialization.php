3eb7e651f14af00265d769b39b9c1924d6ebfbf0
Rob Nugen <rob@robnugen.com>
2021-11-20 12:00:28 +0900

    Add bubbles table to migration

--- resources/migrations/20210228195050_initialization.php
+++ resources/migrations/20210228195050_initialization.php
@@ -6,6 +6,19 @@ class Initialization extends AbstractMigration
 {
     protected function up(): void
     {
+        $this->table('bubbles', 'bubble_id')
+            ->setCharset('utf8mb4')
+            ->setCollation('utf8mb4_unicode_ci')
+            ->addColumn('bubble_id', 'integer', ['autoincrement' => true])
+            ->addColumn('category', 'tinyinteger', ['length' => 4])
+            ->addColumn('cx', 'integer')
+            ->addColumn('cy', 'integer')
+            ->addColumn('radius', 'smallinteger', ['length' => 6])
+            ->addColumn('fill', 'integer')
+            ->addColumn('created_by_id', 'integer', ['null' => true, 'length' => 255])
+            ->addIndex('created_by_id', '', 'btree', 'created_by_id')
+            ->create();
+
         $this->table('users', 'user_id') /* was id in the Slim 4 Skeleton, but I prefer user_id to make JOINs easier (if written manually) */
             ->setCharset('utf8mb4')
             ->setCollation('utf8mb4_unicode_ci')
@@ -25,6 +38,9 @@ class Initialization extends AbstractMigration
 
     protected function down(): void
     {
+        $this->table('bubbles')
+            ->drop();
+
         $this->table('users')
             ->drop();
     }
